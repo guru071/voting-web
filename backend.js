d9f21a448cfa6be6ea103cc95d7cc98cd6a2b3ef
guru071 <guruprasath23062008@gmail.com>
2026-02-10 18:07:01 +0530

    updated

--- backend.js
+++ backend.js
@@ -1,47 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./database');
+document.addEventListener("DOMContentLoaded", () => {
+  const nextBtn = document.getElementById("nextBtn");
 
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// serve html file
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/datastore.html');
-});
-
-// handle form submit
-app.post('/submit', (req, res) => {
-  const {
-    voter_name,
-    voter_id,
-    contact_no,
-    aadhar_no,
-    birth_date,
-    age
-  } = req.body;
-
-  const sql = `
-    INSERT INTO vote
-    (voter_name, voter_id, contact_no, aadhar_no, birth_date, age)
-    VALUES (?, ?, ?, ?, ?, ?)
-  `;
-
-  db.query(
-    sql,
-    [voter_name, voter_id, contact_no, aadhar_no, birth_date, age],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.send('Error saving data');
-      }
-      res.send('Form submitted successfully ✅');
-    }
-  );
-});
-
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+  if (nextBtn) {
+    nextBtn.addEventListener("click", () => {
+      window.location.href = "aadhar.html";
+    });
+  }
 });
