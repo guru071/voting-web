90b1b3f3c502f17744f996799abfd48a5112c62c
guru071 <guruprasath23062008@gmail.com>
2026-02-27 09:12:34 +0530

    Delete politics_j.js

--- politics_j.js
+++
@@ -1,5 +0,0 @@
-polictics_name=["tvk","ntk","dmk","admk","nota","pmk"];
-const table=document.getElementsByClassName("table_");
-for(i=0;i<polictics_name.length;i++){
-    table.innerHTML +="<tr><td>${polictics_name}</td><td></td><td></td></tr>";
-}
