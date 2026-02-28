db7b66cad0ba57e8a0d98de37ddaede81d699686
guru071 <guruprasath23062008@gmail.com>
2026-02-28 11:14:19 +0530

    Add files via upload

--- form.js
+++ form.js
@@ -25,7 +25,9 @@ async function insertdata() {
         message.innerHTML = "<span class='error'>All fields are required!</span>";
         return;
     }
-
+    if (aadharNo.length === 12 && !isNaN(aadharNo)) {
+        alert("Enter vaild aadhar");
+    }
     // AGE CHECK
     const today = new Date();
     const birthDate = new Date(birth);
